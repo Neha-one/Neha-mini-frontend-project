1a2d976b16a97469d5b75bd484cb54d763385673
Neha-one <justforgtb@gmail.com>
2025-11-17 17:53:32 +0530

    delete these files

--- 09-Random-Color-Generator/exercise.js
+++
@@ -1,5 +0,0 @@
-// Ques 01 Create a h2 element with text "hello js" . append "from neha" to this text using js.
-let h2 = document.querySelector(".h2-heading");
-console.log(h2.innerText);      //Hello js!
-h2.innerText = h2.innerText + " Neha";
-console.log(h2.innerText);      //Hello js! Neha
