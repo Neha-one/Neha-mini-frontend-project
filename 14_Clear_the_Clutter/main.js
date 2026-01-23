6246eb49ccac7c6d211378fd1c5a3bef36d0ff46
Neha-one <justforgtb@gmail.com>
2026-01-23 10:28:17 +0530

    update these files

--- 14_Clear_the_Clutter/main.js
+++ 14_Clear_the_Clutter/main.js
@@ -8,25 +8,41 @@ fs.readdir("clutterdFiles", (err, files) => {
         console.log("error", err);
         return;
     }
-    // console.log(files);
 
     files.forEach((file) => {
 
-        // console.log(path.extname(file));
         const oldPath = path.join("clutterdFiles", file);
         const extn = path.extname(file);
 
         if (extn == ".pdf") {
+            //if specific files of pdf not pre made then:
+            // 1st we check is it available or not:
+            if (!fs.existsSync('pdfFiles')) {
+                //if not available then create it.
+                fs.mkdirSync('pdfFiles')
+            };
             const newPath = path.join("pdfFiles", file);
             fs.renameSync(oldPath, newPath);
-        }
+        };
 
         if (extn == ".png") {
+            //if specific files of png not pre made then:
+            // 1st we check is it available or not:
+            if (!fs.existsSync('pngFiles')) {
+                //if not available then create it.
+                fs.mkdirSync('pngFiles')
+            };
             const newPath = path.join("pngFiles", file);
             fs.renameSync(oldPath, newPath);
         }
 
         if (extn == ".jpg") {
+            //if specific files of jpg not pre made then:
+            // 1st we check is it available or not:
+            if (!fs.existsSync('jpgFiles')) {
+                //if not available then create it.
+                fs.mkdirSync('jpgFiles')
+            };
             const newPath = path.join("jpgFiles", file);
             fs.renameSync(oldPath, newPath);
         }
