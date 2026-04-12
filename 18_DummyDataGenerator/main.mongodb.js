25ea893a58fee7ba06d0f8b22a2d16f6c8bae417
Neha-one <justforgtb@gmail.com>
2026-04-12 16:33:12 +0530

    update this file

--- 18_DummyDataGenerator/main.mongodb.js
+++ 18_DummyDataGenerator/main.mongodb.js
@@ -4,24 +4,28 @@ import { employees } from './models/data.js'
 import { random } from './models/callingFun.js'
 import { RandomBool } from './models/callingFun.js'
 import { Name, City, Language } from './models/callingFun.js'
+// this line is used to import environment variables from .env file. you can also directly write url in main.mongodb.js file but it is not recommended for security purpose.
 import "dotenv/config";
 
 
 const app = express()
 const port = 3000;
 
+//this is  mongodb connection url which is stored in .env file for security purpose. you can also directly write url here but it is not recommended.
 const URL = process.env.MONGO_URL
 const conn = () => {
     mongoose.connect(`${URL}/company`)
 }
 
+//this line is used to set ejs as view engine for rendering html file. you can also use other view engines like pug, handlebars etc. but here we are using ejs for simplicity.
 app.set('view engine', 'ejs');
 
+//this line is used to serve static files from src folder. you can also use other folders like public, assets etc. but here we are using src for simplicity.
 app.use(express.static('src'));
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+// app.get('/', (req, res) => {
+//     res.render('index');
+// })
 
 app.get('/generate', async (req, res) => {
     await employees.deleteMany({});
@@ -38,7 +42,10 @@ app.get('/generate', async (req, res) => {
         await newData.save();
         getDataForPrint.push(newData)
     }
-    res.json({ message: "Data Generated Successfully", data: getDataForPrint });
+    res.render('index');
+
+    //if you want to print data in console then use below line and comment above line.
+    // res.json({ message: "Data Generated Successfully", data: getDataForPrint });
 }
 )
 
